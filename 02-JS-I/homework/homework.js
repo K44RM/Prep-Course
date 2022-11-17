f56d0e8d99305e4b650820cc0ce43a643a2dfb68
Juan <luraschi89@gmail.com>
2022-11-17 04:03:59 -0300

    Ahora si

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -174,7 +174,7 @@ function esPositivo(numero) {
     return false;
 
   } else if (numero > 0) {
-    return "es positivo";
+    return "Es positivo";
   } else {
     return "Es negativo";
   }
@@ -202,7 +202,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + nombre + "!";
+  return "Hola " + nombre + "!";
   
 }
 
