890896d47c2624956457cd1b54dee24acba73a3f
Juan <luraschi89@gmail.com>
2022-11-17 02:22:15 -0300

    Resolviendo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -36,26 +36,27 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  return x+y
-  
+  return x + y
+
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x-y
+  return x - y
+
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
-}return x*y
+  return x * y
+}
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return x/y
+  return x / y
 }
 
 function sonIguales(x, y) {
@@ -72,6 +73,11 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (str1.lenght === str2.lenght) {
+    return "true"
+  } else {
+    return "false"
+  }
   
 }
 
@@ -79,14 +85,22 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90) {
+    return "true"
+  } else {
+    return "false"
+  }
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {
+    return "true"
+  } else {
+    return "false"
+  }
 }
 
 function obtenerResto(x, y) {
