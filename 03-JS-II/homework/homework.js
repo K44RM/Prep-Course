796862e5733e689972ce4128f2e923ef2d7c46f9
Juan <luraschi89@gmail.com>
2022-11-18 04:13:41 -0300

    Numeros primos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -46,16 +46,13 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if (idioma === "aleman"){
+  if (idioma === "aleman") {
     return "Guten Tag!";
-  }
-  else if (adioma === "mandarin"){
+  } else if (idioma === "mandarin") {
     return "Ni Hao!" ;
-  }
-  else if (adioma === "ingles"){
+  } else if (idioma === "ingles") {
 return "Hello!";
-  }
-  else {
+  } else {
     return "Hola";
   }
   
@@ -69,7 +66,7 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  switch (color){
+  switch (color) {
     case "blue":
       return "This is blue";
     case "red":
@@ -78,12 +75,9 @@ function colors(color) {
       return "This is green";
     case "orange":
       return "This is orange";
-      default:
-        "Color not found"
-    
-
-
-  }
+     default:
+        "Color not found";
+    }
 }
 
 function esDiezOCinco(numero) {
@@ -129,6 +123,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 > 0 && num1 > num2 && num1 > num3){
+    return "Número 1 es mayor y positivo"
+  }
+  else if (num1 < 0 || num2 < 0 || nume3 < 0){
+    return "Hay negativos"
+  }
+  else if(num3 > num1 && num3 > num2){
+    return num3 + 1;
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+  }
+  else{
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -137,6 +146,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
